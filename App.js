84171219e20d279278e5883b2d72567be8f48960
Kelsey Duncan <Kelsey_Duncan@ultimatesoftware.com>
2018-06-04 17:06:49 -0400

    add shadow to button and use openSans fonts

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, Alert, Image, TouchableOpacity } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      fontLoaded: false,
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     };
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'openSansRegular': require('./assets/fonts/openSansRegular.ttf'),
+      'openSansLight': require('./assets/fonts/openSansLight.ttf'),
+      'openSansSemiBold': require('./assets/fonts/openSansSemiBold.ttf'),
+      'openSansBold': require('./assets/fonts/openSansBold.ttf'),
+    });
+    this.setState({ fontLoaded: true });
   }
   render() {
     return (
@@ -20,25 +31,29 @@ export default class App extends React.Component {
           opacity: 0.15
         }}>
         <Image style={{ flex: 1 }}
-          source={require('./assets/fg-splash-bg.jpg')}/>
+          source={require('./assets/images/fg-splash-bg.jpg')}/>
         </View>
         <View style={styles.container}>
           <Image style={{width: 300, height: 80}}
             transform={[{scaleX: 0.4}, {scaleY: 0.4}]}
-            source={require('./assets/logo-transparent.png')} />
-          <View style={styles.content}>
-            <Text style={styles.title}>Start Your Club</Text>
-            <Text style={styles.info}>{this.state.text}</Text>
-            <View>
-              <TouchableOpacity style={styles.button}
-                color="#FFFFFF"
-                onPress={() => {
-                  Alert.alert('You tapped the button! You go Glen Coco!');
-                }}>
-                <Text style={styles.buttonText}>Get Started</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+            source={require('./assets/images/logo-transparent.png')} />
+            {
+              this.state.fontLoaded ? (
+              <View style={styles.content}>
+                <Text style={styles.title}>Start Your Club</Text>
+                <Text style={styles.info}>{this.state.text}</Text>
+                <View>
+                  <TouchableOpacity style={styles.button}
+                    color="#FFFFFF"
+                    onPress={() => {
+                      Alert.alert('You tapped the button! You go Glen Coco!');
+                    }}>
+                    <Text style={styles.buttonText}>Get Started</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+              ) : null
+            }
         </View>
       </View>
     );
@@ -59,10 +74,12 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end'
   },
   title: {
-    fontSize: 32,
+    fontFamily: 'openSansLight',
+    fontSize: 40,
     color: '#818282'
   },
   info: {
+    fontFamily: 'openSansSemiBold',
     fontSize: 14,
     color: '#818282',
     textAlign: 'center',
@@ -76,10 +93,14 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     alignItems: 'center',
     backgroundColor: '#F313B7',
+    shadowRadius: 8,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.4
   },
   buttonText: {
+    fontFamily: 'openSansBold',
     fontSize: 18,
-    fontWeight: 'bold',
     color: '#FFFFFF'
   }
 });
