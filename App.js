03799f4e7d980b5bc0b0e155ee204cf1bfe8b2a3
Molly Merritt <molly_merritt@ultimatesoftware.com>
2018-06-04 13:51:39 -0400

    typescript udpates

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+import React, { Component } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+interface Props {} // 1
+interface State { // 2
+    name: string
 }
-
+export default class App extends Component<Props, State> { // 3
+    state = { name: 'Nader' } // 4
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    Welcome {this.state.name}! // 5
+                </Text>
+            </View>
+        )
+    }
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    }
+})
