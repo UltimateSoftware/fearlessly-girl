7b0f3c7c65ddb58322ebbb9f62111284f20dfdab
Neela Balkaran <neela_balkaran@ultimatesoftware.com>
2018-06-05 11:31:54 -0400

    Add auth

--- pages/UserSignup.js
+++ pages/UserSignup.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, View, Image } from 'react-native';
+import { Button, View, Image, Alert } from 'react-native';
 import styles from '../component/styles';
 import { AuthSession } from 'expo';
 
@@ -24,35 +24,46 @@ export default class UserSignup extends React.Component {
     _loginWithAuth0 = async () => {
         const redirectUrl = AuthSession.getRedirectUrl();
         console.log(`Redirect URL: ${redirectUrl}`);
-        const result = await AuthSession.startAsync({
-            authUrl: `${auth0Domain}/authorize` + toQueryString({
-                client_id: auth0ClientId,
-                response_type: 'token',
-                scope: 'openid name',
-                redirect_uri: redirectUrl,
-            }),
-        });
-
-        console.log(result);
-        if (result.type === 'success') {
-            this.handleParams(result.params);
+        try {
+            const result = await AuthSession.startAsync({
+                authUrl: `${auth0Domain}/authorize` + toQueryString({
+                    client_id: auth0ClientId,
+                    response_type: 'token',
+                        scope: 'openid name',
+                    redirect_uri: redirectUrl,
+                }),
+            });
+            console.log(result);
+            if (result.type === 'success') {
+                this.handleParams2(result.params);
+            }
+        } catch(error) {
+            console.log("Error ", error);
         }
     };
 
-    handleParams = (responseObj) => {
+    handleParams2 = (responseObj) => {
         if (responseObj.error) {
             Alert.alert('Error', responseObj.error_description
                 || 'something went wrong while logging in');
             return;
         }
-        const encodedToken = responseObj.id_token;
-        const decodedToken = jwtDecoder(encodedToken);
-        const username = decodedToken.name;
-        this.setState({ username });
+
+        fetch(`${auth0Domain}/userinfo?access_token=${responseObj.access_token}`)
+            .then(response => {
+                if (response.status === 200) {
+                    response.json().then(parsedResponse => {
+                        console.log("UserID", parsedResponse);
+                    })
+                }
+                else {
+                    console.log('Something went wrong. ErrorCode: ', response.status);
+                }
+            })
     };
 
-    componentWillMount(){
-        this._loginWithAuth0
+    async componentWillMount() {
+        this._loginWithAuth0();
     }
 
     render() {
@@ -66,9 +77,7 @@ export default class UserSignup extends React.Component {
                 <Image style={{width: 300, height: 80}} 
                 transform={[{scaleX: 0.4}, {scaleY: 0.4}]} 
                 source={require('../assets/images/logo-transparent.png')} />
-            <View style={styles.content}>
             </View>
-          </View>
         </View>
       );
     }
